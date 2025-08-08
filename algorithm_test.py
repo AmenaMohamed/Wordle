730877df73c86dd830e80f1cd8e411a5fc6c2eed
Amena Mohamed <amenamohhassan19@gmail.com>
2025-08-08 22:08:40 +0300

    Add algorithm_test.py

--- algorithm_test.py
+++ algorithm_test.py
@@ -1,11 +1,21 @@
+#constant
+N= 4
 word=input("word:")
-output =""
+output=[0]*N
 guess=input("guess:")
-for i in word:
-    if guess.find(word)==i:
-        output[i]= 1 #found in correct pos
-    elif guess.find(word)==-1 :
-        output[i] = 0 #not found
-    else: 
+R=[0]*N
+
+for i in range(N):
+    R[i]= guess.count(word[i]) 
+    #count the repeation of each letter , assign it to it's index 
+
+for i in range(N):
+    if word.find(guess[i])== -1 :
+        output[i]= 0 #not found
+    elif word.find(guess[i])== i :
+        output[i] = 1 #found in correct pos
+    elif R[i]!= 0:
         output[i] = 2 #found in incorrect pos
+        R[i]-=1 # we found one of the repeations & coded it
+
 print(output)
