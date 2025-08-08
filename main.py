1221277d0512e5543ff5df05b738c2ce439f4c71
Amena Mohamed <amenamohhassan19@gmail.com>
2025-08-08 16:24:35 +0300

    Add main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+words = []
+with open('words.txt', 'r') as file:
+    for line in file:
+        words.append(line) # Add words from the current line to the main list
+
+for item in words:
+    print(item)
