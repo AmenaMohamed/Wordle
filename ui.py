a8e57576564ab991fb82c414f7f4d6d312ba49ac
Amena Mohamed <amenamohhassan19@gmail.com>
2025-08-11 22:16:22 +0300

    Add ui.py

--- ui.py
+++ ui.py
@@ -9,22 +9,27 @@ new_wordlist = []  # list to store all guesses with colors(list of words)
 
 def UI(guesslist, outputlist):
     for guess, output in zip(guesslist, outputlist):
-
+        
          for i in range(len(guess)):
             # decoding each output in the output list into colours
+            if guess=="":
+                print("_") # if no guess print empty spaces
+            else:
+                letter= ""
+                match output[i]:
+                    case 0:
+                        # colour the letter grey
+                        letter += "\033[90m" + guess[i].upper()
 
-            match output[i]:
-                case 0:
-                    # colour the letter grey
-                    letter += "\033[90m" + guess[i].upper()
+                    case 1:
+                        # colour the letter green
+                        letter += "\033[32m" + guess[i].upper()
 
-                case 1:
-                    # colour the letter green
-                    letter += "\033[32m" + guess[i].upper()
-
-                case 2:
-                    # colour the letter yellow
-                    letter += "\033[33m" + guess[i].upper()
+                    case 2:
+                        # colour the letter yellow
+                        letter += "\033[33m" + guess[i].upper()
 
             print(letter)  # join the letters to form the guess
-
+            print(" ")  # space between letters
+            new_word.append(letter)
+         new_wordlist.append(new_word) #append mutted guesses to a list
